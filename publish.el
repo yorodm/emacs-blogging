cdb398b54234d811e1d31040dd10a5da5cbda84d
Sachin Patil <psachin@redhat.com>
2020-07-17 10:29:04 +0530

    Add html template for pre/postamble
    - Making posts ready for tags
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- publish.el
+++ publish.el
@@ -1,5 +1,5 @@
 ;; publish.el --- Publish org-mode project on Gitlab Pages
-;; Author: Sachin
+;; Author: Sachin Patil <iclcoolster@gmail.com, psachin@redhat.com>
 
 ;;; Commentary:
 ;; This script will convert the org-mode files in this directory into
@@ -29,13 +29,13 @@
       org-export-with-smart-quotes t
       org-export-with-toc nil)
 
-(defvar psachin-date-format "%b %d, %Y")
+(defvar this-date-format "%b %d, %Y")
 
 (setq org-html-divs '((preamble "header" "top")
                       (content "main" "content")
                       (postamble "footer" "postamble"))
       org-html-container-element "section"
-      org-html-metadata-timestamp-format psachin-date-format
+      org-html-metadata-timestamp-format this-date-format
       org-html-checkbox-type 'html
       org-html-html5-fancy t
       org-html-validation-link t
@@ -44,7 +44,7 @@
       org-src-fontify-natively t)
 
 
-(defvar psachin-website-html-head
+(defvar me/website-html-head
   "<link rel='icon' type='image/x-icon' href='/images/favicon.jpg'/>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link rel='stylesheet' href='https://code.cdn.mozilla.net/fonts/fira.css'>
@@ -52,44 +52,24 @@
 <link rel='stylesheet' href='/css/custom.css' type='text/css'/>
 <link rel='stylesheet' href='/css/syntax-coloring.css' type='text/css'/>")
 
-(defun psachin-website-html-preamble (plist)
+(defun me/website-html-preamble (plist)
   "PLIST: An entry."
-  ;; Skip adding subtitle to the post if :KEYWORDS don't have 'post' has a
-  ;; keyword
-  (when (string-match-p "post" (format "%s" (plist-get plist :keywords)))
-    (plist-put plist
-	       :subtitle (format "Published on %s by %s."
-				 (org-export-get-date plist psachin-date-format)
-				 (car (plist-get plist :author)))))
+  (if (org-export-get-date plist this-date-format)
+        (plist-put plist
+             :subtitle (format "Published on %s by %s."
+                               (org-export-get-date plist this-date-format)
+                               (car (plist-get plist :author)))))
+  ;; Preamble
+  (with-temp-buffer
+    (insert-file-contents "../html-templates/preamble.html") (buffer-string)))
 
-  ;; Below content will be added anyways
-"<div class='intro'>
-<img src='/images/about/profile.png' alt='Sachin Patil' class='no-border'/>
-<h1>Sachin</h1>
-<p>Free Software developer & Emacser</p>
-</div>
+(defun me/website-html-postamble (plist)
+  "PLIST."
+  (concat (format
+           (with-temp-buffer
+             (insert-file-contents "../html-templates/postamble.html") (buffer-string))
+           (format-time-string this-date-format (plist-get plist :time)) (plist-get plist :creator))))
 
-<div class='nav'>
-<ul>
-<li><a href='/'>Blog</a>.</li>
-<li><a href='http://gitlab.com/psachin'>GitLab</a>.</li>
-<li><a href='http://github.com/psachin'>GitHub</a>.</li>
-<li><a href='https://www.reddit.com/user/psachin'>Reddit</a>.</li>
-<li><a href='https://youtube.com/user/iclcoolsterU'>YouTube</a>.</li>
-<li><a href='/index.xml'>RSS</a>.</li>
-<li><a href='/about/'>About</a></li>
-</ul>
-</div>")
-
-(defvar psachin-website-html-postamble
-  "<div class='footer'>
-Copyright © 2012-2020 <a href='mailto:iclcoolster@gmail.com'>Sachin Patil</a> | <a href='https://gitlab.com/psachin/psachin.gitlab.io'>Source</a><br>
-GnuPG fingerprint: <a href='https://pgp.key-server.io/0xE5F9CE4862AA06E2'>28C5 A1F3 221B 949D B651 FC47 E5F9 CE48 62AA 06E2</a> <br>
-Adapted from <a href='https://nicolas.petton.fr'>https://nicolas.petton.fr</a> <br>
-Last updated on %C using %c <br>
-<a href='http://www.catb.org/hacker-emblem/'> <img src='http://www.catb.org/hacker-emblem/glider.png' alt='hacker emblem' /></a>
-<a href='https://info.flagcounter.com/tlzk'><img src='https://s11.flagcounter.com/mini/tlzk/bg_FFFFFF/txt_9E9E9E/border_FFFFFF/flags_0/' alt='Free counters!' border='0'></a>
-</div>")
 
 (defvar site-attachments
   (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
@@ -97,7 +77,7 @@ Last updated on %C using %c <br>
   "File types that are published as static files.")
 
 
-(defun psachin/org-sitemap-format-entry (entry style project)
+(defun me/org-sitemap-format-entry (entry style project)
   "Format posts with author and published data in the index page.
 
 ENTRY: file-name
@@ -109,13 +89,13 @@ PROJECT: `posts in this case."
                  entry
                  (org-publish-find-title entry project)
                  (car (org-publish-find-property entry :author project))
-                 (format-time-string psachin-date-format
+                 (format-time-string this-date-format
                                      (org-publish-find-date entry project))))
         ((eq style 'tree) (file-name-nondirectory (directory-file-name entry)))
         (t entry)))
 
 
-(defun psachin/org-reveal-publish-to-html (plist filename pub-dir)
+(defun me/org-reveal-publish-to-html (plist filename pub-dir)
   "Publish an org file to reveal.js HTML Presentation.
 FILENAME is the filename of the Org file to be published.  PLIST
 is the property list for the given project.  PUB-DIR is the
@@ -134,16 +114,16 @@ publishing directory. Returns output file name."
          :auto-sitemap t
          :sitemap-filename "index.org"
          :sitemap-title "Blog Index"
-         :sitemap-format-entry psachin/org-sitemap-format-entry
+         :sitemap-format-entry me/org-sitemap-format-entry
          :sitemap-style list
          :sitemap-sort-files anti-chronologically
          :html-link-home "/"
          :html-link-up "/"
          :html-head-include-scripts t
          :html-head-include-default-style nil
-         :html-head ,psachin-website-html-head
-         :html-preamble psachin-website-html-preamble
-         :html-postamble ,psachin-website-html-postamble)
+         :html-head ,me/website-html-head
+         :html-preamble me/website-html-preamble
+         :html-postamble me/website-html-postamble)
         ("about"
          :base-directory "about"
          :base-extension "org"
@@ -156,9 +136,9 @@ publishing directory. Returns output file name."
          :html-link-up "/"
          :html-head-include-scripts t
          :html-head-include-default-style nil
-         :html-head ,psachin-website-html-head
-         :html-preamble psachin-website-html-preamble
-         :html-postamble ,psachin-website-html-postamble)
+         :html-head ,me/website-html-head
+         :html-preamble me/website-html-preamble
+         :html-postamble me/website-html-postamble)
         ("gureSagardoa"
          :base-directory "gureSagardoa"
          :base-extension "org"
@@ -172,9 +152,9 @@ publishing directory. Returns output file name."
          :html-link-up "/"
          :html-head-include-scripts t
          :html-head-include-default-style nil
-         :html-head ,psachin-website-html-head
-         :html-preamble psachin-website-html-preamble
-         :html-postamble ,psachin-website-html-postamble)
+         :html-head ,me/website-html-head
+         :html-preamble me/website-html-preamble
+         :html-postamble me/website-html-postamble)
         ("photography"
          :base-directory "photography"
          :base-extension "org"
@@ -187,9 +167,9 @@ publishing directory. Returns output file name."
          :html-link-up "/"
          :html-head-include-scripts t
          :html-head-include-default-style nil
-         :html-head ,psachin-website-html-head
-         :html-preamble psachin-website-html-preamble
-         :html-postamble ,psachin-website-html-postamble)
+         :html-head ,me/website-html-head
+         :html-preamble me/website-html-preamble
+         :html-postamble me/website-html-postamble)
         ("horology"
          :base-directory "horology"
          :base-extension "org"
@@ -197,7 +177,7 @@ publishing directory. Returns output file name."
          :auto-sitemap t
          :sitemap-filename "index.org"
          :sitemap-title "Horology"
-         :sitemap-format-entry psachin/org-sitemap-format-entry
+         :sitemap-format-entry me/org-sitemap-format-entry
          :sitemap-sort-files anti-chronologically
          :recursive nil
          :publishing-function org-html-publish-to-html
@@ -206,16 +186,16 @@ publishing directory. Returns output file name."
          :html-link-up "/horology"
          :html-head-include-scripts t
          :html-head-include-default-style nil
-         :html-head ,psachin-website-html-head
-         :html-preamble psachin-website-html-preamble
-         :html-postamble ,psachin-website-html-postamble)
+         :html-head ,me/website-html-head
+         :html-preamble me/website-html-preamble
+         :html-postamble me/website-html-postamble)
         ("slides"
          :base-directory "slides"
          :base-extension "org"
          :exclude ,(regexp-opt '("README.org" "draft"))
          :index-filename "index.org"
          :recursive t
-         :publishing-function psachin/org-reveal-publish-to-html
+         :publishing-function me/org-reveal-publish-to-html
          :publishing-directory "./public/slides/")
         ("css"
          :base-directory "./css"
